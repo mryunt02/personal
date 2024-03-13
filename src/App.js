30871d2bf29ce9f283a0173c7d2106f9ab28a505
mryunt02 <mrthresh58@gmail.com>
2024-03-13 22:34:36 +0300

    last (I hope)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/personal" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
